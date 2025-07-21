91e02ce61903b78b771ea7ab02e698ade3723a93
SONALSUBODH <ms.sonalkulkarni@gmail.com>
2025-07-21 14:30:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,3 +31,21 @@ tags = var.sg_tags
 data "aws_vpc" "default" {
   default = true
 }
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+output "instance_ip_addr" {
+  value = aws_instance.demo.public_ip
+}
