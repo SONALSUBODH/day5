dfacf655ce3926c9b21caa3ca34fc54a07d1bba0
SONALSUBODH <ms.sonalkulkarni@gmail.com>
2025-07-21 14:15:24 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,33 @@
+resource "aws_security_group" "tf_sg" {
+  name        = "var.sg_name"
+  description = "var.sg_description"
+  vpc_id      = "var.vpc_id"
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    
+  }
+ ingress {
+    description = "TLS from VPC"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+tags = var.sg_tags
+}
+
+
+data "aws_vpc" "default" {
+  default = true
+}
